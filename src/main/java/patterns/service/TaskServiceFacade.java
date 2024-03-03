46ca71e49f3ffb81d3dba4da05d6db0d0936997c
Anton <pakhorukov.a@mail.ru>
2024-03-04 04:37:44 +0700

    work_12

---
+++ src/main/java/patterns/service/TaskServiceFacade.java
@@ -0,0 +1,14 @@
+package patterns.service;
+
+import patterns.model.ITask;
+
+import java.util.List;
+
+public interface TaskServiceFacade {
+    String createTask(Long id, String urgency);
+
+    List<ITask> getUrgencyList();
+
+    List<ITask> getNotUrgencyList();
+
+}
